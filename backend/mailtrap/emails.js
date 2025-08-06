64163e1a35e81795be482243d275c1f88d611ff6
manishbisht2000 <manishbisht2000@gmail.com>
2025-08-06 17:33:18 +0530

    Verify, welcome email & logout

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -15,8 +15,29 @@ export const sendVerificationEmail = async (email, verificationToken) => {
 
         console.log("Email sent successfully", response)
     } catch (error) {
-        console.log(`Error sending verification`, error)
+        console.error(`Error sending verification`, error)
 
         throw new Error(`Error sending verification email: ${email}`)
+    }
+}
+
+export const sendWelcomeEmail = async(email,name) => {
+    const recipient = [{ email }]
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            template_uuid:"1018abce-45d4-409a-8f9d-8c1e3768bd41",
+            template_variables: {
+                company_info_name: "Auth Company",
+      name: name
+            }
+        })
+
+        console.log("Welcome email sent sucessfully", response)
+    } catch (error) {
+        console.error(`Error sending welcome email`, error)
+        throw new Error(`Error sending welcome email: ${error}`)
     }
 }
