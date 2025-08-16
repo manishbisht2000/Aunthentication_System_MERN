d90d3070e9a7c3da14db512d81ce99ad9b5fde6f
manishbisht2000 <manishbisht2000@gmail.com>
2025-08-16 20:39:09 +0530

    Reset password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcryptjs from "bcryptjs"
 import { User } from "../models/user.model.js"
 import crypto from "crypto"
 import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js"
-import { sendPasswordResetEmail, sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js"
+import { sendPasswordResetEmail, sendResetSuccessEmail, sendVerificationEmail, sendWelcomeEmail } from "../mailtrap/emails.js"
 
 export const signup = async(req, res) => {
     const {email, password, name} = req.body
@@ -141,5 +141,36 @@ export const forgotPassword  = async(req, res) => {
     } catch (error) {
         console.log("Error in forgot password ", error)
         res.status(400).json({success:false, message: error.message})
+    }
+}
+
+export const resetPassword = async(req, res) => {
+    try {
+        const {token} = req.params
+        const {password} = req.body
+        
+        const user = await User.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpiresAt: { $gt:Date.now() }
+        })
+
+        if(!user){
+            return res.status(400).json({success:false, message: "Expired or reset expired token"})
+        }
+
+        // update password
+        const hashedPassword = await bcryptjs.hash(password, 10)
+
+        user.password = hashedPassword
+        user.resetPasswordToken = undefined
+        user.resetPasswordExpiresAt = undefined
+        await user.save()
+
+        await sendResetSuccessEmail(user.email)
+
+        res.status(200).json({success:true, message: "Password reset successful"})
+    } catch (error) {
+        console.log("Error in resetPassword", error)
+        res.status(400).json({success: false, message: error.message})
     }
 }
