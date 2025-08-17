5f29d67ef87d8f5d231300ad2d6ab425c8b43400
manishbisht2000 <manishbisht2000@gmail.com>
2025-08-17 22:35:48 +0530

    Check auth endpoints for reset password

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from "dotenv"
+import cookieParser from 'cookie-parser'
 
 import {connectDB} from './db/connectDB.js'
 
@@ -10,7 +11,8 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
+app.use(express.json()) //allows us to parse imcoming requests:req.body
+app.use(cookieParser()) //allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes)
 
