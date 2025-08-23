19f453d7b34033e7b9211d5934154a92262b15fa
manishbisht2000 <manishbisht2000@gmail.com>
2025-08-23 12:36:20 +0530

    Signup page UI- Password strength meter

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,6 +3,7 @@ import Input from '../components/Input'
 import { Lock, Mail, User } from 'lucide-react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import PasswordStrengthMeter from '../components/PasswordStrengthMeter'
 
 const SignUpPage = () => {
     const [name, setName] = useState("")
@@ -50,7 +51,9 @@ const SignUpPage = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                {/* Password strength meter */}
+                
+                <PasswordStrengthMeter password={password} />
+
                 <motion.button
                     className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
 						font-bold rounded-lg shadow-lg hover:from-green-600
